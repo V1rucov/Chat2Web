bbd967974f48d56a35629cce1ff97d3eba545d9e
Witalij Ruzhnikov <rujnikov2015@outlook.com>
2024-11-23 08:13:48 +0800

    final v1.0, tcp support added

--- src/main/java/org/Chat2Web/Chat2Web/ChatMessage.java
+++ src/main/java/org/Chat2Web/Chat2Web/ChatMessage.java
@@ -1,8 +1,7 @@
 package org.Chat2Web.Chat2Web;
-
 import java.util.Date;
 
-public class ChatMessage {
+public class ChatMessage  {
     public String Message;
     public String Username;
     public Date date;
--- src/main/java/org/Chat2Web/Chat2Web/Senders/TCPSender.java
+++ src/main/java/org/Chat2Web/Chat2Web/Senders/TCPSender.java
@@ -1,11 +1,27 @@
 package org.Chat2Web.Chat2Web.Senders;
-
 import org.Chat2Web.Chat2Web.ChatMessage;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class TCPSender implements ISender {
 
     @Override
     public void Send(String address, ChatMessage msg) {
+        String[] addr = address.split(":");
+        String serialized = "{\"Username\":\""+msg.Username+"\", \"date\":\""+msg.date.toString()+"\", \"Message\":\""+msg.Message+"\"}";
+
+        try {
+            Socket sock = new Socket(addr[0], Integer.parseInt(addr[1]));
+            OutputStream outputStream = sock.getOutputStream();
+            PrintWriter printWriter = new PrintWriter(outputStream, true);
+            printWriter.write(serialized);
+        } catch (Exception e) {
+            System.out.println("O-oh! Something went wrong with tcp connection...");
+        }
 
     }
 }
